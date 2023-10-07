35a2751ae64ea06ce6c6b037aea5691f7d7ca7d8
Shihab <hello@shihabiiuc.com>
2023-10-08 02:23:48 +0600

    completed landing page

--- js/script.js
+++ js/script.js
@@ -43,3 +43,19 @@ const animatedElements = document.querySelectorAll('.animated-element');
 animatedElements.forEach((element) => {
   observer.observe(element);
 });
+
+// ACCORDION
+var accdr = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < accdr.length; i++) {
+  accdr[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}

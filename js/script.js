8a7a6e72f70d18596be85c9d9d82cafdaadafdd3
Shihab <hello@shihabiiuc.com>
2023-10-08 02:43:35 +0600

    removed animate.css

--- js/script.js
+++ js/script.js
@@ -1,21 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
   const hamburgers = document.querySelectorAll('.hamburger')
-  const mobileNavs = document.getElementsByClassName('nav-links')[0]
-  const openMobileNavAnimation = document.querySelectorAll('.nav-links ul')[0]
-  
+  const mobileNavs = document.getElementsByClassName('nav-links')[0]  
 
   for (var i = 0; i < hamburgers.length; i++) {
     hamburgers[i].addEventListener('click', function() {
       this.classList.toggle('active')
       mobileNavs.classList.toggle('active')
-
-      // adding animate.css classes
-      var animateMobileNav = function(e, c1, c2, c3) {
-        openMobileNavAnimation.classList.toggle(c1)
-        openMobileNavAnimation.classList.toggle(c2)
-        openMobileNavAnimation.classList.toggle(c3)
-      }
-      animateMobileNav(openMobileNavAnimation,'animate__animated', 'animate__fadeIn', 'animate__slow')
     })
   }
 })
